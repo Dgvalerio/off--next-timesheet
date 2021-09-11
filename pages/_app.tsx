013e3b52a058c194a371263d5efcb168c61c2ed9
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-11 13:07:55 -0300

    :sparkles: feat(code): Code format
    Adding dependencies to format and padronize code.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { NextPage } from 'next';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => (
+  <Component {...pageProps} />
+);
+export default MyApp;
