b45c8d7eebf8383384785ec5f2133c021fa8964d
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-09-18 12:42:25 -0300

    :white_check_mark: test(navigation): show home page

--- cypress/integration/app.spec.ts
+++ cypress/integration/app.spec.ts
@@ -3,9 +3,8 @@ const urlGen = (url: string): string => `http://localhost:3000${url}`;
 describe('Navigation', () => {
   it('should show home page', () => {
     cy.visit('/');
-
-    cy.get('h1').contains('Welcome to Next.js!');
-
     cy.url().should('eq', urlGen('/'));
+
+    cy.get('h1').contains('Home');
   });
 });
